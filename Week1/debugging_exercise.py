aaa7867541ed65868f558a4f33aeca7a8991adc1
kirstymcl1 <kirstymclarnon@live.co.uk>
2023-03-22 13:23:34 +0000

    added guess tracker
    added variable to count number of guesses

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -3,10 +3,13 @@ import random
 # pick a random number for the user to guess
 rand = random.randint(1, 20)
 
+number_of_guesses = 0
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
+number_of_guesses +=1
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
+    number_of_guesses += 1
     if guess > rand:  # if the guess is too high, tell the user.
         print('Too high. Guess again.')
     else:  # if the guess is too low, tell the user.
@@ -16,3 +19,4 @@ while guess != rand:  # if the guess is not equal to the random number, you have
     guess = int(input())
 
 print('You got it! The number was {}'.format(rand))
+print ('You guessed the number in ' + str(number_of_guesses) + ' attempts!')
