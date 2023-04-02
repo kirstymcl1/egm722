38dec44a3fb84ced627d614ef866da6ea33d549c
kirstymcl1 <kirstymclarnon@live.co.uk>
2023-04-02 16:05:03 +0100

    Update exercise_script.py

--- Week3/exercise_script.py
+++ Week3/exercise_script.py
@@ -20,14 +20,13 @@ print(join)
 join_total = join['Population'].sum()
 print(join_total)
 print(join.groupby(['CountyName'])['Population'].sum())
+
 # ---------------------------------------------------------------------------------------------------------------------
 # below here, you may need to modify the script somewhat to create your map.
 # create a crs using ccrs.UTM() that corresponds to our CRS
 myCRS = ccrs.UTM(29)
 # create a figure of size 10x10 (representing the page size in inches
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
-myFig = plt.figure(figsize=(10, 10))
-ax = plt.axes(projection=myCRS)
 # add gridlines below
 gridlines = ax.gridlines(draw_labels=True,
                          xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],
@@ -49,6 +48,5 @@ ax.add_feature(county_outlines)
 county_handles = [mpatches.Rectangle((0, 0), 1, 1, facecolor='none', edgecolor='r')]
 
 ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
-myFig
 # save the figure
 fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
